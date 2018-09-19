52da077ad68097cf6e4b2b2ee334d256b966d97c
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-19 19:45:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser = 'Grace Hopper';
 var welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
 var excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + "!";
-var shortGreeting = 'Welcome, ' + currentUser + "!";
+var shortGreeting = 'Welcome, ' + 'G' + "!";
