b3d7b6a74670f303d2e25c77d1a4a33f2c5500a5
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-19 19:42:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
 var welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
-var welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
+var excitedWelcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
